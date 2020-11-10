d1d57de9747d250415e799c8157f4fd61034daf7
unknown <henochmanampiring@ymail.com>
2020-11-10 23:46:00 +0700

    Edit UserWasteList css

--- src/components/UserWasteList.vue
+++ src/components/UserWasteList.vue
@@ -4,21 +4,21 @@
       <v-card
         class="mx-auto card-waste"
       >
-        <v-container>
-          <v-row>
-            <v-col class="col-img">
+        <div class="div-card-container">
+          <div class="div-card-row">
+            <div class="div-img">
               <v-img class="img-product"
                      src="https://cdn.vuetifyjs.com/images/cards/store.jpg"></v-img>
-            </v-col>
-            <v-col class="col-content">
-              <v-list-item-content>
-                <div class="overline mb-4">OVERLINE</div>
-                <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
-                <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-col>
-          </v-row>
-        </v-container>
+            </div>
+            <div class="col-content">
+              <div class="div-waste-content">
+                <div class="judul-content">Plastik Botol Bening</div>
+                <div class="judul-content">27 Agustus 2020</div>
+                <div class="judul-content">5 Kg</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </v-card>
     </div>
   </div>
@@ -33,30 +33,10 @@ export default {
 <style lang="scss" scoped>
   @import "../styles/basics/variables";
 
-  .card-waste{
-    border: $border-card-color 2px solid;
-    border-radius: 20px;
-  }
-
-  .img-product{
-    width: 100px;
-    height: 100px;
-    border-radius: 20px;
-  }
-
-  .col-img{
-    display: flex;
-    justify-content: center;
-  }
-
-  .col-content{
-
-  }
-
   .card-list{
     overflow-x: scroll;
     white-space: nowrap;
-    height: 200px;
+    height: 130px;
     scrollbar-width: none;
   }
 
@@ -69,4 +49,45 @@ export default {
     width: 300px;
     margin-right: 10px;
   }
+
+  .card-waste{
+    border: $border-card-color 2px solid;
+    border-radius: 20px;
+    height: 120px;
+  }
+
+  .div-card-container{
+  }
+
+  .div-card-row{
+    display: flex;
+    flex-direction: row;
+    padding: 10px 5px;
+  }
+
+  .div-img{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100px;
+    height: 100px;
+    overflow: hidden;
+  }
+
+  .img-product{
+    max-width: 100px;
+    max-height: 100px;
+    border-radius: 20px;
+  }
+
+  .col-content{
+    display: flex;
+    width: 150px;
+  }
+
+  .div-waste-content{
+    padding: 8px 0;
+    margin-left: 10px;
+  }
+
 </style>
