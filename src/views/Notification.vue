f75ae7dda3716dd761d4a8eeac92695311bc37b9
unknown <henochmanampiring@ymail.com>
2020-12-29 23:44:34 +0700

    add review and notification page

---
+++ src/views/Notification.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container-page">
+      <div class="div-notification-icon">
+        <div class="wrapper-notification-icon">
+          <a class="text-title">
+
+          </a>
+          <div class="div-icon">
+
+          </div>
+        </div>
+      </div>
+      <div class="div-notification-icon">
+        <div class="wrapper-notification-icon">
+          <a class="text-title">
+
+          </a>
+          <div class="div-icon">
+
+          </div>
+        </div>
+      </div>
+      <div class="div-notification-icon">
+        <div class="wrapper-notification-icon">
+          <a class="text-title">
+
+          </a>
+          <div class="div-icon">
+
+          </div>
+        </div>
+      </div>
+      <div class="div-notification-icon">
+        <div class="wrapper-notification-icon">
+          <a class="text-title">
+
+          </a>
+          <div class="div-icon">
+
+          </div>
+        </div>
+      </div>
+      mdi-buffer(pesanan baru), mdi-clock-time-nine-outline(Menunggu Konfirmasi), mdi-cube-send(sudah dikirm),
+      mdi-map-marker-radius-outline(Telah Sampai), mdi-sync(siap dikirim) , mdi-check-box-outline(sudah dikirim)
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Notification'
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../styles/pages/notification";
+
+</style>
--- src/views/ReviewAndGreeting.vue
+++ src/views/ReviewAndGreeting.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="container-page">
     <div class="div-img">
-      <img src="../assets/planet.png" alt="conserve-pic">
+      <img src="../assets/8441.webp" alt="conserve-pic">
     </div>
     <div class="div-greeting">
       <span class="span-greeting">
-
+        Terima kasih, anda telah mendaur ulang <a class="highlight-text">{{weight.data}}Kg</a> sampah kategori <a class="highlight-text">{{waste.category}}</a> yang sangat berarti untuk pelestarian lingkungan.
       </span>
     </div>
     <div>
       Ulasan:
     </div>
     <div class="div-star-input">
-
+      <div class="text-xs-center">
+        <v-rating v-model="rating"></v-rating>
+      </div>
     </div>
     <div class="review-input">
-
+      <v-textarea
+        clearable
+        clear-icon="cancel"
+        label="Text"
+        value="This is clearable text."
+      ></v-textarea>
     </div>
     <button>
       Submit
@@ -25,10 +32,16 @@
 
 <script>
 export default {
-  name: 'ReviewAndGreeting'
+  name: 'ReviewAndGreeting',
+  data: function () {
+    return {
+      rating: 4
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+  @import "../styles/pages/reviewAndGreeting";
 
 </style>
